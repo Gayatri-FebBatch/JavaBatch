d24e457416974fd5117f25f171e2723707579034
gayat <gayat@Gayatri>
2023-03-12 09:30:01 -0500

    Testing

--- LocalProject/src/Testing2/Share1.java
+++ LocalProject/src/Testing2/Share1.java
@@ -1,5 +1,9 @@
 package Testing2;
 
-public class Share1 {
-
+public class Share1 
+{
+private void Share1() 
+  {
+	System.out.println("Welcome to all of you");   // TODO Auto-generated method stub
+  }	
 }
